9ab874c32de20c49bf97983d4d034014bbd22180
tputhi <tputhi@cisco.com>
2019-02-19 17:35:21 +0530

    ActorsIntro

--- src/main/scala/Actors/ActorsIntro.scala
+++ src/main/scala/Actors/ActorsIntro.scala
@@ -1,6 +1,6 @@
 package Actors
 
-import akka.actor.{Actor, ActorSystem}
+import akka.actor.{Actor, ActorSystem, Props}
 
 object ActorsIntro extends App {
 
@@ -8,6 +8,48 @@ object ActorsIntro extends App {
   val actorSystem = ActorSystem("firstActorSystem")
   println(actorSystem.name)
 
+  //part2 - create Actors
+  class LeafVillage extends Actor {
+    //internal data
+    var ninjaCount = 0
 
+    //behaviour
+    override def receive: Receive = {
+      case msg:String => {
+      ninjaCount += msg.split(" ").length
+      println(s"The number of ninjas are $ninjaCount")
+      }
+      case _ => println("Unknown ninjas")
+    }
+  }
+
+  //part3 - instanstinate Actors
+  val naruto = actorSystem.actorOf(Props[LeafVillage], "naruto")
+
+  //part4 - send msgs
+  naruto ! "Hinata Sauske Itachi"
+  naruto ! "Sakura Ino"
+
+
+//companion object for methid2
+  object Person {
+  def props(name:String) = Props(new Person(name))
+  }
+
+  //Actors with parameters
+  class Person(name: String) extends Actor {
+    override def receive: Receive = {
+      case msg:String => println(s"Hi my name is $name")
+      case _ => println("Unknow msg")
+    }
+  }
+
+  //instantinate - method 1
+  val person1 = actorSystem.actorOf(Props(new Person("Bob")))
+  person1 ! "Hey"
+
+  //Method2
+  val person2 = actorSystem.actorOf(Person.props("Bob"))
+  person2 ! "Hello"
   }
 
